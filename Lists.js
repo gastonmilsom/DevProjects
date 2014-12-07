62fc5e6e31dc4ffc7c9a74bc3c0726bcd94f47b3
gastonmilsom <gastonm@datacom.co.nz>
2014-12-07 17:49:57 +1300

    Add List file...

---
+++ Lists.js
@@ -0,0 +1,58 @@
+var list = {
+	value: 1, rest: {
+		value: 2, rest: {
+			value: 3, rest: null
+			}
+		}
+	}
+
+
+function printList(list) {
+	var listCrawler = list;
+	while (listCrawler.rest != null) {
+		console.log(listCrawler.value);
+		listCrawler = listCrawler.rest;
+	}
+	console.log(listCrawler.value); //prints the last element in the list (after the While is terminated
+}
+
+function arrayToListB2FWhile(array) {
+	var listPointer;
+	var list = {value: array.pop(), rest: null};
+	listPointer = list;
+	while (array.length > 0) {
+		list = {value: array.pop(), rest: listPointer}
+		listPointer = list;
+	}
+	
+	return listPointer;
+}
+
+function arrayToListB2FFor(array) {
+	var listHead = {};
+	var listPointer;
+	var i = array.length -1;
+	for (var i=array.length-1;i>=0;i--) {
+		listPointer = listHead;
+		listHead.value = array[i];
+		listHead.rest = listPointer;
+	}
+	return listHead;
+}
+
+function addToHead(list, newValue) {
+	listHead = {value: newValue, rest: list};
+	return listHead;
+}
+	
+
+
+console.log(arrayToListB2FWhile([10,20,30, 40]));
+list = addToHead(list, 10);
+console.log(list);
+
+		
+
+
+
+	
